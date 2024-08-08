1f2e3c10c3e162af2bab69a7283b739d848bd095
AwesomeDocs19 <marahalomari666@gmail.com>
2024-08-07 17:09:12 -0700

    Update ItemManager.java

--- src/main/java/com/awesomedocs19/magicwand/ItemManager.java
+++ src/main/java/com/awesomedocs19/magicwand/ItemManager.java
@@ -5,10 +5,11 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemManager {
-    public static ItemStack createMagicWand() {
+    public static ItemStack createMagicWand(int modelData) {
         ItemStack wand = new ItemStack(Material.STICK);
         ItemMeta meta = wand.getItemMeta();
         meta.setDisplayName("Magic Wand");
+        meta.setCustomModelData(modelData);
         wand.setItemMeta(meta);
         return wand;
     }
